a9daa2629eb1058819cc99651e7805bb107ee47a
Shubhang <97521606+shubhang-cherry@users.noreply.github.com>
2023-10-18 11:40:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+noseX=0 
+noseY=0
+
+function preload () {
+    clown_image=loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png');
+}
+
+function setup() {
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('poseNet is Initialized');
+}
+
+function gotPoses(results) 
+{
+    if(results.length>0) 
+    {
+        noseX=results[0].pose.nose.x-15;
+        noseY=results[0].pose.nose.y-15;
+        console.log(results);
+        console.log('nose x = '+ noseX);
+        console.log('nose y = '+ noseY);
+    }
+}
+
+function draw () {
+    image(video, 0 ,0 ,300,300);
+    image(clown_image,noseX,noseY,30,30);
+}
+function take_snapshot () {
+save('MyFilteredImage.png');
+}
